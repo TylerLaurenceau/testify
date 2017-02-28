dbb11c403abfd678fbf5934a174352c3fc8c5577
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-28 18:13:16 -0500

    got normal, backstage, and brie working

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,79 @@ function GildedRose (sellIn, quality, name) {
   this.quality = quality;
 }
 
+GildedRose.prototype.nameIsNormal = function (){
+  this.quality = this.quality - 1;
+  this.sellIn = this.sellIn - 1;
+  if (this.sellIn <= 0){
+    this.quality = this.quality - 1;
+  }
+  if (this.quality === -1){
+    this.quality = this.quality + 1;
+  }
+}
+
+GildedRose.prototype.nameIsAgedBrie = function (){
+  this.quality = this.quality + 1;
+  this.sellIn = this.sellIn - 1;
+  if (this.sellIn <= 0){
+    this.quality = this.quality + 1;
+  }
+  if (this.quality > 50){
+    this.quality = 50;
+  }
+}
+
+GildedRose.prototype.nameIsBackstage = function (){
+  this.quality = this.quality + 1;
+  this.sellIn = this.sellIn - 1;
+  if (this.quality === 11){
+    this.quality = this.quality +1;
+  }
+  if (this.sellIn === 10){
+    this.quality = this.quality -1;
+  }
+  if (this.sellIn === 4){
+    this.quality = this.quality +1;
+  }
+  if (this.sellIn === 0){
+    this.quality = this.quality +1;
+  }
+  if (this.quality > 50){
+    this.quality = 50;
+  }
+  if (this.sellIn < 0){
+    this.quality = 0;
+  }
+}
+
+GildedRose.prototype.nameIsCake = function (){
+  
+}
+
 GildedRose.prototype.tick = function () {
 
-      if (this.name === "normal" || this.name === "Conjured Mana Cake"){
+  if (this.name === "normal"){
+    this.nameIsNormal();
+    return;
+  }
+
+  if (this.name === "Aged Brie"){
+    this.nameIsAgedBrie();
+    return;
+  }
+
+  if (this.name === "Backstage passes to a TAFKAL80ETC concert"){
+    this.nameIsBackstage();
+    return;
+  }
+
+  if (this.name === "Conjured Mana Cake"){
+    this.nameIsCake();
+    return;
+  }
+
+
+        if (this.name === "normal" || this.name === "Conjured Mana Cake"){
         if (this.quality > 0){
          this.quality = this.quality - 1;
     }
