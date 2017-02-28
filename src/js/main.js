4a45704cad9c425e42638172e347c485de004dad
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-28 18:27:18 -0500

    deleted uglier starting code

--- src/js/main.js
+++ src/js/main.js
@@ -68,6 +68,8 @@ GildedRose.prototype.nameIsCake = function (){
 
 GildedRose.prototype.tick = function () {
 
+//______________________________________________________________________________
+
   if (this.name === "normal"){
     this.nameIsNormal();
     return;
@@ -87,55 +89,5 @@ GildedRose.prototype.tick = function () {
     this.nameIsCake();
     return;
   }
-
-
-        if (this.name === "normal" || this.name === "Conjured Mana Cake"){
-        if (this.quality > 0){
-         this.quality = this.quality - 1;
-    }
  }
-  else {
-    if (this.quality < 50) {
-      this.quality = this.quality + 1;
-      if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.sellIn < 11) {
-          if (this.quality < 50) {
-            this.quality = this.quality + 1;
-          }
-        }
-        if (this.sellIn < 6) {
-          if (this.quality < 50) {
-            this.quality = this.quality + 1;
-          }
-        }
-      }
-    }
-  }
-  if (this.name === 'Conjured Mana Cake' && this.quality === 9){
-    this.quality = this.quality - 1;
-  }
-  if (this.name != 'Sulfuras, Hand of Ragnaros') {
-    this.sellIn = this.sellIn - 1;
-  }
-  if (this.sellIn < 0) {
-    if (this.name != 'Aged Brie') {
-      if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.quality > 0) {
-          if (this.name != 'Sulfuras, Hand of Ragnaros') {
-            this.quality = this.quality - 1;
-          }
-        }
-      } else {
-        this.quality = this.quality - this.quality;
-      }
-    } else {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1;
-      }
-    }
-  }
-  if (this.name === 'Conjured Mana Cake' && this.quality === 7){
-      this.quality = this.quality -1;
-    }
-}
 export { GildedRose };
